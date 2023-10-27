50293488f12462fcb3a246f01b7b602b61e57c2a
Keila Amada <keila.amadap@hotmail.com>
2023-10-27 11:43:34 -0300

    comment out unused variables for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// import Image from 'next/image'
+// import styles from './page.module.css'
 import Main from './components/Main'
 
 export default function Home() {
